21b2993ae3c92c1d30bc6e5907078a24cf7d1a90
wang laosi <40877394+bigd4@users.noreply.github.com>
2020-03-26 20:03:23 +0800

    fix conflict

--- magus/molecule.py
+++ magus/molecule.py
@@ -142,35 +142,6 @@ class Molfilter:
         atoms = Atoms(symbols=symbols,positions=positions,pbc=self.atoms.pbc,cell=self.atoms.cell)
         return atoms
         
-<<<<<<< HEAD
-#TODO steal from ase, need to change
-def isolate_components(atoms, kcutoff=None): 
-    if kcutoff is None:
-        intervals = analyze_dimensionality(atoms, method='RDA', merge=False)
-        m = intervals[0]
-        if m.b == float("inf"):
-            kcutoff = m.a + 0.1
-        else:
-            kcutoff = (m.a + m.b) / 2
-    data = {}
-    components, all_visited, ranks = traverse_graph(atoms, kcutoff)
-
-    for k, v in all_visited.items():
-        if v is None:
-            continue
-        v = sorted(list(v))
-        key = tuple(np.unique([c for c, offset in v]))
-        for c in key:
-            components[np.where(components == c)] = key[0]
-            if c in all_visited.keys():
-                all_visited[c] = None
-    unique_components = np.unique(components)
-    sort_components = np.arange(len(unique_components))
-    d = {old_tag:new_tag for old_tag,new_tag in zip(unique_components,sort_components)}
-    for i,old_tag in enumerate(components):
-        components[i] = d[old_tag]
-    return components
-=======
 # #TODO from ase, need to change
 # def isolate_components(atoms, kcutoff=None): 
 #     if kcutoff is None:
@@ -194,7 +165,6 @@ def isolate_components(atoms, kcutoff=None):
 #                 all_visited[c] = None
 
 #     return components
->>>>>>> dev
 
 
 if __name__ == '__main__':
