41c1ba91459c35c4e022dd6724bb2b4d00ff32c6
潘书宁 <161120094@smail.nju.edu.cn>
2021-10-30 10:06:25 +0800

    Update summary.py

--- magus/entrypoints/summary.py
+++ magus/entrypoints/summary.py
@@ -21,19 +21,16 @@ def summary(*args, filenames=[], prec=0.1, remove_features=[], add_features=[], 
         for atoms in frames:
             atoms.info['source'] = filename.split('.')[0]
         all_frames.extend(frames)
-    show_features = [feature for feature in ['symmetry', 'enthalpy', 'parentE', 'origin', 'fullSym', 'priSym']\
+    show_features = [feature for feature in ['symmetry', 'enthalpy', 'parentE', 'origin', 'fullSym', 'priSym', 'Eo', 'energy']\
             if feature not in remove_features]
     show_features.extend(add_features)
     if len(filenames) > 1 and 'source' not in show_features:
         show_features.append('source')
     if sorted_by.lower() in ['', 'none']:
         sorted_by = None
-    else:
-        try:
-            key_index = show_features.index(sorted_by)
-        except:
-            print('{} not in show features, auto choose enthalpy as sort feature'.format(sorted_by))
-            key_index = show_features.index('enthalpy')
+    if sorted_by not in show_features:
+        print('{} not in show features, auto choose enthalpy as sort feature'.format(sorted_by))
+        sorted_by = "enthalpy"
     for i, atoms in enumerate(all_frames):  
         if cluster:
             molecule = Molecule(atoms.symbols,atoms.get_positions())
@@ -43,26 +40,37 @@ def summary(*args, filenames=[], prec=0.1, remove_features=[], add_features=[], 
         atoms.info['cellpar'] = np.round(atoms.cell.cellpar(), 2).tolist()
         atoms.info['lengths'] = atoms.info['cellpar'][:3]
         atoms.info['angles'] = atoms.info['cellpar'][3:]
-        lattice, scaled_positions, numbers = spg.find_primitive(atoms, prec)
-        pri_atoms = Atoms(cell=lattice, scaled_positions=scaled_positions, numbers=numbers)
-        atoms.info['priSym'] = pri_atoms.get_chemical_formula()
-        volume = atoms.get_volume()
-        atoms.info['volume'] = round(volume, 3)
+        atoms.info['volume'] = round(atoms.get_volume(), 3)
         atoms.info['fullSym'] = atoms.get_chemical_formula()
+
+        # sometimes spglib cannot find primitive cell.
+        try:
+            lattice, scaled_positions, numbers = spg.find_primitive(atoms, prec)
+            pri_atoms = Atoms(cell=lattice, scaled_positions=scaled_positions, numbers=numbers)
+        except:
+            # if fail to find prim, set prim to raw
+            print("Fail to find primitive for structure {}".format(i))
+            pri_atoms = atoms
+        finally:
+            atoms.info['priSym'] = pri_atoms.get_chemical_formula()
+
         atoms.info['row'] = [atoms.info[feature] if feature in atoms.info.keys() else None \
                              for feature in show_features]
+    
+    alldata = [atoms.info['row'] for atoms in all_frames]
+    df = pd.DataFrame(alldata, columns=show_features)
     if sorted_by is not None:
-        all_frames.sort(key=lambda atoms:atoms.info['row'][key_index] or 100)
+        df = df.sort_values(by=[sorted_by,])
+
     if save:
         if not os.path.isdir(outdir):
             os.makedirs(outdir)
-        for i, atoms in enumerate(all_frames):
+        for i, index in enumerate(df.index):
             posname = os.path.join(outdir, "POSCAR_{}.vasp".format(i+1))
-            write(posname, atoms, direct = True, vasp5 = True)
-    alldata = [atoms.info['row'] for atoms in all_frames[:show_number]]
-    df = pd.DataFrame(alldata, columns=show_features)
-    df.index += 1
+            write(posname, all_frames[index], direct = True, vasp5 = True)
+
+    df.index = range(1, len(df)+1)
     if reverse:
-        print(df[::-1])
+        print(df[:-show_number:-1])
     else:
-        print(df)
+        print(df[:show_number])
