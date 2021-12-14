4e534e14754208cf54f3e719a1038a3a95b7425a
个个君 <141120108@smail.nju.edu.cn>
2021-12-14 19:17:44 +0800

    add requirement

--- magus/__init__.py
+++ magus/__init__.py
@@ -1,4 +1,4 @@
-__version__ = 'devel'
+__version__ = '1.0.4'
 __picture__ = """ 
             ,'  , `.     MAGUS                                           
         ,-+-,.' _ |      (v.{} RELEASE)                                    
