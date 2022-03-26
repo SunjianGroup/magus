79468e2f8e1570460eac09570a1757834d590ad6
Gege Jun <141120108@smail.nju.edu.cn>
2022-03-26 20:25:17 +0800

    Specify the order of comparators

--- magus/comparators/__init__.py
+++ magus/comparators/__init__.py
@@ -11,14 +11,15 @@ log = logging.getLogger(__name__)
 def get_comparator(p_dict):
     comparators = {
         'connect': 'or', 
-        'naive': {},
-        'ase-zurek': {},
+        'comparator_list': ['naive', 'ase-zurek'],
         }
     if 'Comparator' in p_dict:
         comparators.update(p_dict['Comparator'])
     comparator_list = []
-    for comparator_name, para in comparators.items():
-        if comparator_name == 'connect':
-            continue
-        comparator_list.append(COMPARATOR_PLUGIN[comparator_name](**{**p_dict, **para}))
+    for comparator_name in comparators['comparator_list']:
+        if comparator_name in comparators:
+            p_dict_ = {**p_dict, **comparators[comparator_name]}
+        else:
+            p_dict_ = {**p_dict}
+        comparator_list.append(COMPARATOR_PLUGIN[comparator_name](**p_dict_))
     return COMPARATOR_CONNECT_PLUGIN[comparators['connect']](comparator_list)
