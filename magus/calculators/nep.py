929a8a9d75bac789438884bd0156e850ea326b7c
萌萌四 <1239612746@qq.com>
2023-03-20 12:46:14 +0800

    nep-noselect

--- magus/calculators/nep.py
+++ magus/calculators/nep.py
@@ -15,6 +15,14 @@ from magus.populations.populations import Population
 log = logging.getLogger(__name__)
 
 
+@CALCULATOR_PLUGIN.register('nep-noselect')
+class PyNEPCalculator(ASECalculator):
+    def __init__(self, **parameters):
+        super().__init__(**parameters)
+        self.relax_calc = NEP("{}/nep.txt".format(self.input_dir))
+        self.scf_calc = NEP("{}/nep.txt".format(self.input_dir))
+
+
 @CALCULATOR_PLUGIN.register('nep')
 class PyNEPCalculator(ASECalculator, ClusterCalculator):
     def __init__(self, **parameters):
