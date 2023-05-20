37d191b408614785e90a36bc2157297603555a02
qhjia <qhjia@smail.nju.edu.cn>
2023-05-21 01:48:04 +0800

    change version number to 1.6.1

--- magus/__init__.py
+++ magus/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.6.0'
+__version__ = '1.6.1'
 __picture__ = """ 
             ,'  , `.     MAGUS                                           
         ,-+-,.' _ |      (v.{} RELEASE)                                    
