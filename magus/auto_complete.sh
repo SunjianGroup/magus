2f0eb12c07df271f6fc1b5dea4c8971b1e233717
qhjia <qhjia@smail.nju.edu.cn>
2024-03-26 16:23:35 +0800

    modify manual and auto_complete

--- magus/auto_complete.sh
+++ magus/auto_complete.sh
@@ -1,8 +1,8 @@
 _magus_complete()
 {
-    local file_list=('summary' 'relax')
+    local file_list=('summary' 'calculate')
     if [[ $COMP_CWORD == 1 ]]; then 
-        COMPREPLY=($(compgen -W 'summary search relax prepare clean' -- ${COMP_WORDS[COMP_CWORD]}))
+        COMPREPLY=($(compgen -W 'summary search calculate prepare clean' -- ${COMP_WORDS[COMP_CWORD]}))
     elif [[ ${file_list[@]} =~ ${COMP_WORDS[1]} ]]; then
         compopt -o filenames
         COMPREPLY=($(compgen -f -- ${COMP_WORDS[COMP_CWORD]}))
